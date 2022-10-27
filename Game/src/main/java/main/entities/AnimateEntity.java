65787baf9388f3d70bb9e7b967e3b2cf84825173
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-10-27 15:25:00 -0700

    Implemented AnimateEntity

---
+++ Game/src/main/java/main/entities/AnimateEntity.java
@@ -0,0 +1,31 @@
+/*  
+ *  AnimateEntity.java
+ *  
+ *  Description: Abstract class, inherites Entity and has movespeed.
+ * 
+ *  Author: Lionel
+ * 
+ *  Last changed: Oct 27th, 2022
+ *
+*/
+
+package main.entities;
+
+abstract class AnimateEntity extends Entity
+{
+    // Attributes
+    private int moveSpeed;
+
+    // Default constructor (must pass X/Y coordinates and speed)
+    public AnimateEntity(int setX, int setY, int setSpeed)
+    {
+        super(setX, setY);
+        this.moveSpeed = setSpeed;
+    }
+
+    // Getters
+    public int get_moveSpeed() { return this.moveSpeed; }
+
+    // Setters
+    public void set_moveSpeed(int val) { this.moveSpeed = val; }
+}
