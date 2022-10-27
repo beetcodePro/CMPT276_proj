2d606a6b39c5252de73873a9329c06cfefe00266
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-10-27 14:55:34 -0700

    Implemented Entity class

---
+++ Game/src/main/java/main/entities/Entity.java
@@ -0,0 +1,38 @@
+/*  
+ *  Entity.java
+ *  
+ *  Description: Abstract class, has a coordinate with corresponding get/set methods.
+ * 
+ *  Author: Lionel
+ * 
+ *  Last changed: Oct 27th, 2022
+ *
+*/
+
+package main.entities;
+
+abstract class Entity 
+{
+    // Attributes
+    private Coordinate position;
+
+    // Default constructor (must pass X & Y coordinates)
+    public Entity(int setX, int setY)
+    {
+        this.position = new Coordinate(setX, setY);
+    }
+
+    // Getters
+    public Coordinate get_coordinate() { return this.position; }
+    public int get_coordinate_X() { return this.position.get_X(); }
+    public int get_coordinate_Y() { return this.position.get_Y(); }
+
+    // Setters
+    public void set_coordinate_X(int setX) { this.position.set_X(setX); }
+    public void set_coordinate_Y(int setY) { this.position.set_Y(setY); }
+    public void set_coordinate(int setX, int setY) 
+    {
+        this.position.set_X(setX);
+        this.position.set_Y(setY);
+    }
+}
