4a5207ef977b8cf664a67495800ac054b8ccfbcd
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-10-27 16:44:27 -0700

    Implemented Player class with basic functions

--- Game/src/main/java/main/entities/Player.java
+++ Game/src/main/java/main/entities/Player.java
@@ -3,27 +3,45 @@
  *  
  *  Description: Placeholder
  * 
- *  Author: Lionel
+ *  Author: Lionel (msg me if u have any questions about this class)
+ * 
+ *  Last changed: Oct 27th, 2022
  *
 */
 
 package main.entities;
+import java.awt.*;
+import main.GamePanel;
+import main.KeyBoard;
 
-public class Player 
+public class Player extends AnimateEntity
 {
     // Attributes
+    private GamePanel sim;
+    private KeyBoard keyboard;
     private int score;
     private int lives;
-    private int rewardsCollected;
-    // private Main simulator
-    // private Coordinate endGoal
+    // private int rewardsCollected;
+    // private Coordinate endGoal;
 
-    // Default constructor
-    public Player()
+    // Default constructor (must pass specified parameters, sets moveSpeed to default)
+    public Player(GamePanel setSim, KeyBoard setKey, int setX, int setY)
     {
+        super(setX, setY);
+        this.sim = setSim;
+        this.keyboard = setKey;
         this.score = 0;
         this.lives = 0;
-        this.rewardsCollected = 0;
+    }
+
+    // Parameterized constructor (must pass specified parameters)
+    public Player(GamePanel setSim, KeyBoard setKey, int setX, int setY, int setSpeed)
+    {
+        super(setX, setY, setSpeed);
+        this.sim = setSim;
+        this.keyboard = setKey;
+        this.score = 0;
+        this.lives = 0;
     }
 
     // Getters
@@ -33,6 +51,43 @@ public class Player
     // Setters
     public void change_score(int change) { this.score = this.score + change; }
     public void change_lives(int change) { this.lives = this.lives + change; }
+
+    // Update player movement
+    public void update()
+    {
+        if (keyboard.PressedRT == true)
+        {
+            int x = this.get_coordinate_X();
+            x = x + this.get_moveSpeed();
+            this.set_coordinate_X(x);
+        }
+        if (keyboard.PressedLF == true)
+        {
+            int x = this.get_coordinate_X();
+            x = x - this.get_moveSpeed();
+            this.set_coordinate_X(x);
+        }
+        if (keyboard.PressedUp == true)
+        {
+            int y = this.get_coordinate_Y();
+            y = y - this.get_moveSpeed();
+            this.set_coordinate_Y(y);
+        }
+        if (keyboard.PressedDown == true)
+        {
+            int y = this.get_coordinate_Y();
+            y = y + this.get_moveSpeed();
+            this.set_coordinate_Y(y);
+        }
+    }
+
+    // Draw player on user interface
+    public void draw(Graphics2D g2)
+    {
+        g2.setColor(Color.GRAY);
+        // FIXME: change last two parameters to this.sim.get_tileSize() after testing
+        g2.fillRect(this.get_coordinate_X(), this.get_coordinate_Y(), 48, 48);
+    }
 
     // Checks if player has no lives left
     public boolean check_if_no_lives()
@@ -44,18 +99,18 @@ public class Player
     }
 
     // TODO: move entity by keyboard input, calls check_tile()
-    public void move_entity();
+    // public void move_entity();
 
     // TODO: checks if tile has been occupied by another entity
-    public string check_tile();
+    // public string check_tile();
 
     // TODO: pauses player input if game is paused
-    public boolean is_game_paused();
+    // public boolean is_game_paused();
 
     // TODO: checks if all rewards are collected on the map
-    public boolean all_rewards_collected();
+    // public boolean all_rewards_collected();
 
     // TODO: checks if player coordinate is equal to endGoal
-    public boolean goal_reached();
+    // public boolean goal_reached();
 }
 
