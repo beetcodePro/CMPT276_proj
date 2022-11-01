44a13ad23bc5eadd846db3b944a90525b270a72a
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-01 15:46:47 -0700

    Moved hitbox methods to Entity class

--- Game/src/main/java/entities/AnimateEntity.java
+++ Game/src/main/java/entities/AnimateEntity.java
@@ -5,12 +5,11 @@
  * 
  *  Author: Lionel (msg me if u have any questions about this class)
  * 
- *  Last changed: Oct 31st, 2022
+ *  Last changed: Nov 1st, 2022
  *
 */
 
 package entities;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import main.Simulator;
 import main.CheckCollision;
@@ -21,14 +20,12 @@ public abstract class AnimateEntity extends Entity
     private int spriteCnt = 0;
     private int spriteNum = 1;
     private int moveSpeed = 4;
-    private boolean canCollide = false;
 
     // Constructed attributes
     protected Simulator sim;
     protected CheckCollision collideCheck;
     protected BufferedImage down1, down2, down3, up1, up2, up3, right1, right2, left1, left2;
     private String direction;
-    private Rectangle hitbox;
 
     // Default constructor (must pass X/Y coordinates and sets moveSpeed to default)
     public AnimateEntity(int setX, int setY, Simulator setSim, CheckCollision setCol)
@@ -37,7 +34,6 @@ public abstract class AnimateEntity extends Entity
         this.sim = setSim;
         this.collideCheck = setCol;
         this.direction = "down";
-        this.config_hitbox();
     }
 
     // Getters
@@ -45,29 +41,12 @@ public abstract class AnimateEntity extends Entity
     public String get_direction() { return this.direction; }
     public int get_spriteCnt() { return this.spriteCnt; }
     public int get_spriteNum() { return this.spriteNum; }
-    public Rectangle get_hitbox() { return this.hitbox; }
-    public boolean get_canCollide() { return this.canCollide; }
 
     // Setters
     public void set_moveSpeed(int val) { this.moveSpeed = val; }
     public void set_direction(String val) { this.direction = val; }
     public void set_spriteCnt(int val) { this.spriteCnt = val; }
     public void set_spriteNum(int val) { this.spriteNum = val; }
-    public void set_hitbox(Rectangle val) { this.hitbox = val; }
-    public void set_hitbox_x(int val) { this.hitbox.x = val; }
-    public void set_hitbox_y(int val) { this.hitbox.y = val; }
-    public void set_canCollide(boolean val) { this.canCollide = val; }
-
-    // Default configure hitbox (called on constructor ONLY)
-    private void config_hitbox()
-    {
-        Rectangle config = new Rectangle();
-        config.x = 0;           // hitbox border width
-        config.y = 0;           // hitbox border height
-        config.width = 48;      // hitbox width
-        config.height = 48;     // hitbox height
-        this.set_hitbox(config);
-    }
 
     // Default update, updates AnimateEntity movement
     public void update()
--- Game/src/main/java/entities/Entity.java
+++ Game/src/main/java/entities/Entity.java
@@ -5,34 +5,55 @@
  * 
  *  Author: Lionel (msg me if u have any questions about this class)
  * 
- *  Last changed: Oct 29th, 2022
+ *  Last changed: Nov 1st, 2022
  *
 */
 
 package entities;
+import java.awt.Rectangle;
 
 public abstract class Entity 
 {
     // Attributes
     private Coordinate position;
+    public Rectangle hitbox;
+    private boolean canCollide = false;
 
     // Default constructor (must pass X & Y coordinates)
     public Entity(int setX, int setY)
     {
         this.position = new Coordinate(setX, setY);
+        this.config_hitbox();
     }
 
     // Getters
     public Coordinate get_coordinate() { return this.position; }
     public int get_coordinate_X() { return this.position.get_X(); }
     public int get_coordinate_Y() { return this.position.get_Y(); }
+    public Rectangle get_hitbox() { return this.hitbox; }
+    public boolean get_canCollide() { return this.canCollide; }
 
     // Setters
     public void set_coordinate_X(int setX) { this.position.set_X(setX); }
     public void set_coordinate_Y(int setY) { this.position.set_Y(setY); }
+    public void set_hitbox(Rectangle val) { this.hitbox = val; }
+    public void set_hitbox_x(int val) { this.hitbox.x = val; }
+    public void set_hitbox_y(int val) { this.hitbox.y = val; }
+    public void set_canCollide(boolean val) { this.canCollide = val; }
     public void set_coordinate(int setX, int setY) 
     {
         this.position.set_X(setX);
         this.position.set_Y(setY);
+    }
+
+    // Default configure hitbox (called on constructor ONLY)
+    private void config_hitbox()
+    {
+        Rectangle config = new Rectangle();
+        config.x = 0;           // hitbox border width
+        config.y = 0;           // hitbox border height
+        config.width = 48;      // hitbox width
+        config.height = 48;     // hitbox height
+        this.set_hitbox(config);
     }
 }
