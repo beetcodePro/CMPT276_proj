f549a1034de93187683132924eb3b57c58faa743
nka63 <nka63@sfu.ca>
2022-11-04 02:56:17 +0000

    added a new UI class for text notifications

---
+++ Game/src/main/java/main/UI.java
@@ -0,0 +1,61 @@
+package main;
+
+import objects.obj_heart;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
+
+public class UI {
+    Simulator sim;
+    Font arial_40;
+    BufferedImage heartImage;
+    public boolean messageOn = false;
+    public String message = "";
+    int messageCounter = 0;
+
+    double playTime;
+    DecimalFormat dFormat = new DecimalFormat("#0.00");
+
+
+    public UI(Simulator gp) {
+        this.sim = gp;
+
+        arial_40 = new Font("Arial", Font.PLAIN, 40);
+        obj_heart heart = new obj_heart();
+        heartImage = heart.image;
+
+    }
+    public void showMessage(String text) {
+        message = text;
+        messageOn = true;
+
+    }
+    public void draw(Graphics2D g2){
+        g2.setFont(arial_40);
+        g2.setColor(Color.white);
+        g2.drawImage(heartImage,sim.tileSize/5, sim.tileSize/5, sim.tileSize, sim.tileSize, null);
+        g2.drawString("x" + sim.player.lives, 60 ,40);
+
+        //Timer
+         playTime +=(double)1/60;
+         g2.drawString("Time:"+ dFormat.format(playTime), sim.tileSize*11,40);
+
+        //message function
+        if(messageOn == true) {
+            g2.setFont(g2.getFont().deriveFont(30F));
+            g2.drawString(message, sim.tileSize/2,sim.tileSize*5);
+
+
+            messageCounter++;
+
+            if(messageCounter > 120){
+                messageCounter = 0;
+                messageOn = false;
+            }
+        }
+    }
+
+}
