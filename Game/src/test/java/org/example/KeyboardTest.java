7ec6416f11b356a77f9eb7c9eed3e1313588ed99
smoradkh <smoradkh@sfu.ca>
2022-11-22 13:18:33 -0800

    added the Keyboard Test

--- Game/src/test/java/org/example/KeyboardTest.java
+++ Game/src/test/java/org/example/KeyboardTest.java
@@ -3,6 +3,8 @@ package org.example;
 import entities.Player;
 import junit.framework.Test;
 import junit.framework.TestCase;
+
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -14,52 +16,72 @@ import main.Simulator;
 
 public class KeyboardTest extends TestCase {
     private Simulator sim;
-    KeyEvent e;
 
 
+
+    /**
+     * Create the test case.
+     * @param testName name of the test case
+     */
 
     public KeyboardTest(String testName)
     {
         super(testName);
+
+
     }
     /**
      * @test for W key on the keyboard
      */
     public void keyPressedUPTest() {
-        e.setKeyChar('W');
-        assertEquals(e.getKeyCode(), KeyEvent.VK_W);
+        Button a = new Button("click");
+        KeyEvent e = new KeyEvent(a, 1, 20, 1, 87, 'W');
+        assertEquals(KeyEvent.VK_W, e.getKeyCode() );
+        KeyEvent e2 = new KeyEvent(a, 1, 20, 1, 38, 'U');
+        assertEquals(KeyEvent.VK_UP, e2.getKeyCode());
+
     }
     /**
      * @test for S key on the keyboard
      */
     public void keyPressedDNTest() {
-        e.setKeyChar('S');
-        assertEquals(e.getKeyCode(), KeyEvent.VK_S);
+        Button a = new Button("click");
+        KeyEvent e = new KeyEvent(a, 1, 20, 1, 83, 'S');
+        assertEquals( KeyEvent.VK_S, e.getKeyCode());
+        KeyEvent e2 = new KeyEvent(a, 1, 20, 1, 40, 'D');
+        assertEquals(KeyEvent.VK_DOWN, e2.getKeyCode());
     }
     /**
      * @test for A key on the keyboard
      */
-    public void keyPressedLFTest() {
-        e.setKeyChar('A');
-        assertEquals(e.getKeyCode(), KeyEvent.VK_A);
+    public void keyPressedLFTest( ) {
+        Button a = new Button("click");
+        KeyEvent e = new KeyEvent(a, 1, 20, 1, 65, 'A');
+        assertEquals(KeyEvent.VK_A, e.getKeyCode());
+        KeyEvent e2 = new KeyEvent(a, 1, 20, 1, 37, 'L');
+        assertEquals(KeyEvent.VK_LEFT, e2.getKeyCode());
     }
     /**
      * @test for D key on the keyboard
      */
-    public void keyPressedRTTest(char C) {
-        e.setKeyChar('D');
-        assertEquals(e.getKeyCode(), KeyEvent.VK_D);
+    public void keyPressedRTTest() {
+        Button a = new Button("click");
+        KeyEvent e = new KeyEvent(a, 1, 20, 1, 68, 'D');
+        assertEquals(KeyEvent.VK_D, e.getKeyCode() );
+        KeyEvent e2 = new KeyEvent(a, 1, 20, 1, 39, 'R');
+        assertEquals(KeyEvent.VK_RIGHT, e2.getKeyCode());
     }
     /**
      * @return the suite of tests being tested
      */
     public static Test suite()
     {
-        TestSuite suite = new TestSuite(KeyboardTest.class);
-        suite.addTest(new KeyboardTest("keyPressedUPTest"));
-        suite.addTest(new KeyboardTest("keyPressedDNTest"));
-        suite.addTest(new KeyboardTest("keyPressedLFTest"));
-        suite.addTest(new KeyboardTest("keyPressedRTTest"));
-        return suite;
+            TestSuite suite = new TestSuite(KeyboardTest.class);
+            suite.addTest(new KeyboardTest("keyPressedUPTest"));
+            suite.addTest(new KeyboardTest("keyPressedDNTest"));
+            suite.addTest(new KeyboardTest("keyPressedLFTest"));
+            suite.addTest(new KeyboardTest("keyPressedRTTest"));
+            return suite;
+
     }
 }
